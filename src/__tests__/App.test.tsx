cfc293e587ac21e3a1faaa8c1572ea69d047ad6f
Paige Watson <paige@industriallogic.com>
2022-10-06 15:42:18 -0700

    use react test library

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render, RenderResult, screen} from '@testing-library/react';
+import App from '../App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const mainDiv: HTMLElement = screen.getByTitle('appTitle');
+  expect(mainDiv.textContent).toEqual('Works!')
+});
